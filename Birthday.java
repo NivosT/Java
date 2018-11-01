5a9a08594837dc840e70e063d3b1086f9f3df01e
Niv <nivtenn@gmail.com>
2018-11-01 21:26:16 +0200

    Update Birthday.java

--- Birthday.java
+++ Birthday.java
@@ -8,18 +8,18 @@ public class Birthday {
 	
 		
 		int n = 2;
-		boolean zeroFive = false;						// Initiating values	
+		boolean zeroFive = false;				// Initiating values	
 														
 			
 			
 		while (!zeroFive) {							
 			
 			zeroFive = (probSomeBirthday(n) > 0.5);		// Runs probability func for n values starting from n=2									
-														// Loop will exit when prob is greater than
+									// Loop will exit when prob is greater than
 			n++;
 			
 			}
-														// Fixes n value for actual vinal result - for the output
+									// Fixes n value for actual vinal result - for the output
 		n--;											
 		
 		
@@ -32,22 +32,24 @@ public class Birthday {
 	
 	
 	/**
-	* Given (N), returns the probability for at least 2 of N to have the same birthday
+	* returns the probability for at least 2 of N to have the same birthday
+	* @param n - amount of people in the room
+	* @return - prob of 2+ of n ppl to have a birthday
 	**/
 	public static double probSomeBirthday(int n) {
 		
-		double pN = 1;									// sets base vars for calc
+		double pN = 1;							// sets base vars for calc
 		double base = 1.0/365.0;
 														
 		for (int i = 1; i<=(n-1); i++) {				// Runs the calc for no birthday withn N people
-														// According to the birthday probability - until N-1		
+										// According to the birthday probability - until N-1		
 														
 			pN *= (1-(base*i));						
 			
 			
 			}
 		
-		double twoMin = 1 - pN;							//calculates final value of min 2+ ppl with same birthday
+		double twoMin = 1 - pN;						//calculates final value of min 2+ ppl with same birthday
 		
 		return twoMin;
 		
