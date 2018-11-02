7b7fbcf802210e1f244c0f4297a661c9a05536b1
Niv <nivtenn@gmail.com>
2018-11-02 14:42:28 +0200

    Add files via upload

---
+++ Calendar.java
@@ -0,0 +1,84 @@
+public class Calendar {	
+	/**
+	* Given year as args[0]:
+	* Prints the number of days in each of the 12 months
+	**/ 
+
+	public static void main(String args[]) {
+		
+		int getYear = Integer.parseInt(args[0]);		// Initiating values
+		String whatYear = "";
+		
+		if (isLeapYear(getYear)) {						// Sets string regarding type of year
+			whatYear = "leap";
+		}
+		else {
+			whatYear = "common";
+		}
+	
+		String output = ("Year " + getYear + " is a " + whatYear + " year");
+		
+		for (int i = 1; i<=12; i++) {					// Builds up output string with month && days no. 
+			int month = i;
+			output += ("\nMonth " + month + " has " + nDaysInMonth(month, getYear) + " days");
+		}
+		
+		System.out.println(output);
+	}
+		 
+	/**
+	* Checks if a given year is a leap year
+	*
+	* @param year - year to answer whether it is leap or not
+	* @return - true/false regarding given year
+	**/
+	private static boolean isLeapYear(int year) {
+														// true  - leap year
+														// false - common year
+		 boolean isLeap = ((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0);
+		 
+		 return isLeap;
+	}
+	 
+	
+	/**
+	* checks the amount of days a given month has
+	* considering the number of month and whether the year is leap
+	*
+	* @param month - number of month (1-12) needed to checked for no. of days
+	* @param year - the year the needed month is in
+	* @return - amount of days of the given month
+	**/
+	private static int nDaysInMonth(int month, int year) {
+		int numDays = 0;
+		
+		switch (month) {
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:										// Months with always 31 days
+			case 10:
+			case 12:
+				numDays = 31;
+				break;
+			case 4:
+			case 6:
+			case 9:										// Months with always 30 days
+			case 11:
+				numDays = 30;
+				break;
+				case 2:
+					if (isLeapYear(year)) {				// Considers leap/not for February
+						numDays = 29;
+					}
+					else {
+						numDays = 28;
+					}
+				break;
+		}	
+		return numDays;
+		
+	}
+	
+}
---
+++ TestRandom.java
@@ -0,0 +1,46 @@
+public class TestRandom {	
+	/**
+	* Given N as args[0]
+	* Runs N pseudo random selections of an int between 0-3
+	* Outputs the amount of selections of each option
+	**/ 
+
+	public static void main(String args[]) {
+		
+		int getN = Integer.parseInt(args[0]);		// Receiving user N choice
+		
+		int zero = 0;								// Initiating count values
+		int one = 0;
+		int two = 0;
+		int three = 0;
+		
+		for (int i = 1; i<=getN; i++){
+			int select = random4();					// Runs N amount of random selections
+													// Logs separately each option
+			if 		(select == 0) zero	+= 1;
+			else if (select == 1) one	+= 1;
+			else if (select == 2) two	+= 1;
+			else if (select == 3) three	+= 1;
+		}
+													// outputs final conclusions
+		System.out.println("We have pseudo-random selected " + getN + " times.");
+		System.out.println("The results are:\n0: " + zero +"\n1: " + one + "\n2: " + two + "\n3: " + three);
+	}
+		
+	 
+	
+	/**
+	* Pseudo random selects an int between 0-3
+	*
+	* @return - int between 0-3
+	**/
+	private static int random4() {
+													 
+		int pseudoSelect = (int)(Math.random()*4);	// Using Math.random()
+													// performs the desired draw
+		return pseudoSelect;
+	}
+		
+}
+	
+
