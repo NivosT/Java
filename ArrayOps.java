a2cdc4753380a919fd6a732a6f56d8cf31b3e1ed
Niv <nivtenn@gmail.com>
2018-11-09 11:04:20 +0200

    Add files via upload

---
+++ ArrayOps.java
@@ -0,0 +1,67 @@
+/** 
+* Assignment number: 3.2
+* File name: ArrayOps.java
+* Name: Niv Tennenbaum
+* Student ID: 312458904
+* Email: niv.tennenbaum@post.idc.ac.il
+*
+* Performs various array operations
+*/
+public class ArrayOps {
+
+	public static void main(String[] args) {
+		
+		//test_printArr();
+		test_maxArr();
+	}	
+	
+	public static void test_printArr() {
+		int[] a = {2,  1,  9,  7,  0};
+		printArr(a); 
+	}
+	
+	public static void test_maxArr() {
+		int[] a = {2,  1,  9,  7,  5};
+		int[] b = {3,  0,  8, 11, -1, 9 ,-2};
+		int[] maxArr = maxArr(a,b);
+		System.out.print("a = "); printArr(a); 
+		System.out.print("b = "); printArr(b); 
+		System.out.print("max = "); printArr(maxArr); 
+	}	
+	
+	/** Returns an array whose elements are the maxima of the respective elements of the 
+	 *  two given arrays. The size of the returned array is the size of the longer array.
+	 *  For example, if the two given arrays are [3,4,1] and [2,6,8,5,1], returns [3,6,8,5,1]. 
+	 */
+	public static int[] maxArr(int[] a, int[] b) {
+
+		int[] maxArr = new int[Math.max(a.length, b.length)];				// Define new array, num of elements as the bigger set
+																			// If sets are equal, that would be the amount
+		for (int i = 0; i <= (Math.min(a.length, b.length) - 1); i++) { 
+			maxArr[i] = Math.max(a[i], b[i]);								// Assigns to new array a corresponding element
+		}																	// the bigger one
+																			// stops at last element of shorter set
+		int[] bigSet = a;
+		int[] smallSet = b;
+		if (b.length > a.length) {
+			bigSet = b;
+			smallSet = a;													// Defining the bigger/smaller arrays
+		}																	
+																			
+		for (int j = (smallSet.length); j <= (bigSet.length - 1); j++) {	// copying the rest of the big array to new array
+			maxArr[j] = bigSet[j];
+		}
+		
+	return maxArr;
+	}
+		
+	/** Prints the given array in one line, with space separators, 
+	 *  and then skips to the next line.
+	 */
+	public static void printArr(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+	    }
+		System.out.println();
+	}
+}
--- Calendar.java
+++ Calendar.java
@@ -1,55 +1,81 @@
+/** 
+* Assignment number: 3.1
+* File name: Calendar.java
+* Name: Niv Tennenbaum
+* Student ID: 312458904
+* Email: niv.tennenbaum@post.idc.ac.il
+*
+* Handles calendars and calendric calculations.
+*/
 public class Calendar {	
-	/**
-	* Given year as args[0]:
-	* Prints the number of days in each of the 12 months
-	**/ 
-
-	public static void main(String args[]) {
-		
-		int getYear = Integer.parseInt(args[0]);		// Initiating values
-		String whatYear = "";
-		
-		if (isLeapYear(getYear)) {				// Sets string regarding type of year
-			whatYear = "leap";
-		}
-		else {
-			whatYear = "common";
-		}
+	// Starting the calendar on 1/1/1900
+	static int dayOfMonth = 1; 
+	static int month = 1;
+	static int year = 1900;
+	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
+	static int nDaysInMonth = 31; // Number of days on January
 	
-		String output = ("Year " + getYear + " is a " + whatYear + " year");
+	/** Prints the calendar and the number of Sundays that fell on the first day of the month
+	 * during the 20th century (from 1/1/1901 till 31/12/2000)
+	 */
+    public static void main(String args[]) {
+		// Advances the date and the day-of-the-week from 1.1.1900 till 31.12.1900 (inclusive).
+		while (year < 1901) {
+			advance();
+		}		
 		
-		for (int i = 1; i<=12; i++) {				// Builds up output string with month && days no. 
-			int month = i;
-			output += ("\nMonth " + month + " has " + nDaysInMonth(month, getYear) + " days");
+		int nSundaysOnFirstOfMonth = 0;
+		
+		// Advances the date and the day-of-the-week throughout the 20th century.
+	    // Counts Sundays that fell of the first of the month.
+		
+		// Implementation tip: except for nSundaysOnFirstOfMonth, this function has no local variables.
+		// All the variables that it uses are the static variables defined at the class level.  
+		
+		while (year < 2001) {
+		
+			if (dayOfWeek == 1 && dayOfMonth == 1) {	// Counts Sunday && 1st of month
+				nSundaysOnFirstOfMonth++;
+			}
+			advance();
 		}
 		
-		System.out.println(output);
-	}
-		 
-	/**
-	* Checks if a given year is a leap year
-	*
-	* @param year - year to answer whether it is leap or not
-	* @return - true/false regarding given year
-	**/
-	private static boolean isLeapYear(int year) {
-									// true  - leap year
-									// false - common year
+		System.out.print("\n" + nSundaysOnFirstOfMonth + " Sundays fell on the first of the month.");
+	 }
+	
+	 // Advances the date and the day-of-the-week.
+	 // If the month changes, sets the number of days in this month.
+	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
+	 private static void advance() {
+		
+		dayOfWeek++;									
+		dayOfMonth++;
+		
+		if (dayOfWeek == 8) {							// Resets week count
+			dayOfWeek = 1;
+		}
+		
+		if (dayOfMonth > nDaysInMonth) {				// Resets month count
+			month++;
+			dayOfMonth = 1;
+			if (month == 13) {
+				year++;
+				month = 1;
+			}
+			nDaysInMonth = nDaysInMonth(month, year);	// Resets amount of days each month
+		} 
+	 }
+	 
+	 /** Returns true if the given year is a leap year, false otherwise. */
+	 public static boolean isLeapYear(int year) {
+														
 		 boolean isLeap = ((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0);
 		 
 		 return isLeap;
-	}
-	 
-	
-	/**
-	* checks the amount of days a given month has
-	* considering the number of month and whether the year is leap
-	*
-	* @param month - number of month (1-12) needed to checked for no. of days
-	* @param year - the year the needed month is in
-	* @return - amount of days of the given month
-	**/
-	private static int nDaysInMonth(int month, int year) {
+	 }
+		 
+	 /** Returns the number of days in the given month, in the given year. */
+	 public static int nDaysInMonth(int month, int year) {
 		int numDays = 0;
 		
 		switch (month) {
@@ -57,28 +83,27 @@ public class Calendar {
 			case 3:
 			case 5:
 			case 7:
-			case 8:						// Months with always 31 days
+			case 8:										// Months with always 31 days
 			case 10:
 			case 12:
 				numDays = 31;
 				break;
 			case 4:
 			case 6:
-			case 9:						// Months with always 30 days
+			case 9:										// Months with always 30 days
 			case 11:
 				numDays = 30;
 				break;
-				case 2:
-					if (isLeapYear(year)) {		// Considers leap/not for February
-						numDays = 29;
-					}
-					else {
-						numDays = 28;
-					}
+			case 2:
+				if (isLeapYear(year)) {				// Considers leap/not for February
+					numDays = 29;
+				}
+				else {
+					numDays = 28;
+				}
 				break;
 		}	
 		return numDays;
 		
-	}
-	
+	 }
 }
