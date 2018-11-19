926349a235c2f286b54743c0c293cd10939b0cdb
Niv <nivtenn@gmail.com>
2018-11-20 00:50:29 +0200

    Update LetterFreq.java

--- LetterFreq.java
+++ LetterFreq.java
@@ -1,42 +1,37 @@
 /** 
-* Assignment number: 4.3
-* File name: LetterFreq.java
-* Name: Niv Tennenbaum
-* Student ID: 312458904
-* Email: niv.tennenbaum@post.idc.ac.il
-*
 * Counts and prints the frequencies of the letters of the English alphabet in a given text.
+* Requires StdIn class file
 */
 public class LetterFreq {
-		// The class has two use cases, described below.
-		public static void main(String[] args) {
-			String text;  // The input text
-			// If the user supplied no command-line argument:
-			if (args.length == 0) {
-				// Reads the text from standard input (typically, a user-supplied text file)
-				text = StdIn.readAll();	// This function reads all the text into one string
-			}
-			// If there is a command-line argument:
-			else {
-				// Reads the text from the command-line argument
-				text = args[0];	
-			}
-			
-			// Computes the frequency of each letter in the English alphabet within the text
-			int[] freqs = frequencies(text);
-			printHistogram(freqs);			
+	// The class has two use cases, described below.
+	public static void main(String[] args) {
+		String text;  // The input text
+		// If the user supplied no command-line argument:
+		if (args.length == 0) {
+			// Reads the text from standard input (typically, a user-supplied text file)
+			text = StdIn.readAll();	// This function reads all the text into one string
 		}
+		// If there is a command-line argument:
+		else {
+			// Reads the text from the command-line argument
+			text = args[0];	
+		}
+			
+		// Computes the frequency of each letter in the English alphabet within the text
+		int[] freqs = frequencies(text);
+		printHistogram(freqs);			
+	}
 		
-		/**
-		 * Counts the frequencies of the 26 letters of the English alphabet in the given string.
-		 * The count is case-insensitive: 'a' and 'A' are treated and counted the same way.
-		 * All the non-alphabetic characters are ignored.
-		 * 
-		 * @param text The text to analyze
-		 * @return An array of frequencies, with 26 entries. Location <i> of the array holds the number of times letter 
-		 * 		   number <i> in the English alphabet appeared in the given text. We consider 'a' to be the 0'th letter.
-		 */
-		public static int[] frequencies(String text) {
+	/**
+	* Counts the frequencies of the 26 letters of the English alphabet in the given string.
+	* The count is case-insensitive: 'a' and 'A' are treated and counted the same way.
+	* All the non-alphabetic characters are ignored.
+	* 
+	* @param text The text to analyze
+	* @return An array of frequencies, with 26 entries. Location <i> of the array holds the number of times letter 
+	* number <i> in the English alphabet appeared in the given text. We consider 'a' to be the 0'th letter.
+	*/
+	public static int[] frequencies(String text) {
 			// The English alphabet has 26 letters
 			final int ALPHABET_SIZE = 26;
 			int[] freqs = new int[ALPHABET_SIZE];
