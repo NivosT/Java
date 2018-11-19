88fea154801587de29532df4b32b316cd1ee9cc0
Niv <nivtenn@gmail.com>
2018-11-20 00:54:32 +0200

    Update LetterFreq.java

--- LetterFreq.java
+++ LetterFreq.java
@@ -1,7 +1,7 @@
 /** 
-* Counts and prints the frequencies of the letters of the English alphabet in a given text.
-* Requires StdIn class file
-*/
+ * Counts and prints the frequencies of the letters of the English alphabet in a given text.
+ * Requires StdIn class file
+ */
 public class LetterFreq {
 	// The class has two use cases, described below.
 	public static void main(String[] args) {
@@ -23,49 +23,48 @@ public class LetterFreq {
 	}
 		
 	/**
-	* Counts the frequencies of the 26 letters of the English alphabet in the given string.
-	* The count is case-insensitive: 'a' and 'A' are treated and counted the same way.
-	* All the non-alphabetic characters are ignored.
-	* 
-	* @param text The text to analyze
-	* @return An array of frequencies, with 26 entries. Location <i> of the array holds the number of times letter 
-	* number <i> in the English alphabet appeared in the given text. We consider 'a' to be the 0'th letter.
-	*/
+	 * Counts the frequencies of the 26 letters of the English alphabet in the given string.
+	 * The count is case-insensitive: 'a' and 'A' are treated and counted the same way.
+	 * All the non-alphabetic characters are ignored.
+	 * 
+	 * @param text The text to analyze
+	 * @return An array of frequencies, with 26 entries. Location <i> of the array holds the number of times letter 
+	 * number <i> in the English alphabet appeared in the given text. We consider 'a' to be the 0'th letter.
+	 */
 	public static int[] frequencies(String text) {
-			// The English alphabet has 26 letters
-			final int ALPHABET_SIZE = 26;
-			int[] freqs = new int[ALPHABET_SIZE];
-			String upperTest = text.toUpperCase(); // Convert all letters to upper case
+		// The English alphabet has 26 letters
+		final int ALPHABET_SIZE = 26;
+		int[] freqs = new int[ALPHABET_SIZE];
+		String upperTest = text.toUpperCase(); // Convert all letters to upper case
 			
-			for (int j = 0; j <= (text.length() - 1); j++) {
-				if ((int) upperTest.charAt(j) >= 65 && (int) upperTest.charAt(j) <= 90) { // Checks only letters
-					freqs[((int) upperTest.charAt(j) - 65)]++; // Increments the letter's index
-					
-				}
+		for (int j = 0; j <= (text.length() - 1); j++) {
+			if ((int) upperTest.charAt(j) >= 65 && (int) upperTest.charAt(j) <= 90) { // Checks only letters
+				freqs[((int) upperTest.charAt(j) - 65)]++; // Increments the letter's index	
 			}
-			return freqs;		
 		}
+		return freqs;		
+	}
 		
-		/**
-		 * Prints the given array of letter frequencies, as a histogram.
-		 * Entry 0 in the array is assumed to hold the number of occurrences of 'a',
-		 * Entry 1 is assumed to hold the number of occurrences of 'b', etc.
-		 * 
-		 * @param frequencies The given array of frequencies
-		 */
-		public static void printHistogram(int[] frequencies) {
+	/**
+	 * Prints the given array of letter frequencies, as a histogram.
+	 * Entry 0 in the array is assumed to hold the number of occurrences of 'a',
+	 * Entry 1 is assumed to hold the number of occurrences of 'b', etc.
+	 * 
+	 * @param frequencies The given array of frequencies
+	 */
+	public static void printHistogram(int[] frequencies) {
 
-			int asciiLower = 97; // Initializes as ascii of a
+		int asciiLower = 97; // Initializes as ascii of a
 			
-			for (int i = 0; i <= (frequencies.length - 1); i++) {
-				System.out.print("\n" + (char) asciiLower + ":"); // Prints line headers of the alphabet
-				asciiLower++;
+		for (int i = 0; i <= (frequencies.length - 1); i++) {
+			System.out.print("\n" + (char) asciiLower + ":"); // Prints line headers of the alphabet
+			asciiLower++;
 				
-				int j = 0;
-				while (j < frequencies[i]) {
-					System.out.print("*"); // 
-					j++;
-				}	
-			}
+			int j = 0;
+			while (j < frequencies[i]) {
+				System.out.print("*"); // Prints histogram for each letter
+				j++;
+			}	
 		}
+	}
 }
